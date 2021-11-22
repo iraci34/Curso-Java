f0427e9b1a083e758bbdc853d778cb5bc43d6910
iraci34 <netinho.iraci@gmail.com>
2021-11-22 14:10:09 -0300

    Consumer 4

--- lambda/src/application/ProgramConsumer.java
+++ lambda/src/application/ProgramConsumer.java
@@ -18,7 +18,9 @@ public class ProgramConsumer {
 		list.add(new Product("Tablet", 350.50));
 		list.add(new Product("HD Case", 80.90));
 		
-		list.forEach(Product::nonStaticPriceUpdate);
+		double factor = 1.1;
+		
+		list.forEach( p -> p.setPrice(p.getPrice() * factor));	
 		
 		list.forEach(System.out::println);
 		
